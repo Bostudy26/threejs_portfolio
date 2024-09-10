d1d462747b37313c7fc564da9a012a99559a32a6
Bo <boprojectbo@gmail.com>
2024-09-10 14:05:53 +0200

    Feat: images/constants database

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,15 +1,53 @@
-const Navbar = () => {
-  return (
-    <header 
-        className='fixed top-0 left-0 right-0 z-50 bg-black/90'
-    >
-        <div className="max-w-7xl mx-auto">
-            <div className="flex justify-between items-center py-5 mx-auto c-space">
+import { useState } from "react"
+import { navLinks } from "../constants";
 
+    const NavItems = () => {
+    return (
+        <ul>
+            {navLinks.map((item) => (
+                <li key={item.id}>
+                    {item.name}
+                </li>
+            ))}
+        </ul>
+    )
+    }
+
+    const Navbar = () => {
+    const [isOpen, setIsOpen] =    useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen((prevIsOpen) => !prevIsOpen);
+    }
+
+    return (
+        <header 
+            className='fixed top-0 left-0 right-0 z-50 bg-black/90'
+        >
+            <div className="max-w-7xl mx-auto">
+                <div className="flex justify-between items-center py-5 mx-auto c-space">
+                    <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
+                        Bo
+                    </a>
+                    <button 
+                        onClick={toggleMenu}
+                        className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
+                        aria-label="toggle menu"    
+                    >
+                        <img 
+                            src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
+                            alt="toggle"
+                            className="w-6 h-6"
+                        />
+                    </button>
+
+                    <nav className="sm:flex hidden" >
+                        <NavItems />
+                    </nav>
+                </div>
             </div>
-        </div>
-    </header>
-  )
-}
+        </header>
+    )
+    }
 
-export default Navbar
+    export default Navbar
