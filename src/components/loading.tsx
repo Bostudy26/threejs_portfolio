3b554d6aaaf042add86c1c9eeed0fdd147382dd3
Bo <boprojectbo@gmail.com>
2024-09-10 16:44:55 +0200

    Feat: eslint  ignore | fix: hackerroom/loading

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -13,9 +13,14 @@ const Loading = () => {
                 flexDirection: "column",
             }}
         >
-            <span className="canvas-loader"  />
+            <span className="canvas-loader"></span>
             <p 
-                style={{ fontSize: 14, color: "#f1f1f1", fontWeight: 800, marginTop: 40 }}
+                style={{ 
+                    fontSize: 14,
+                    color: "#f1f1f1",
+                    fontWeight: 800,
+                    marginTop: 40
+                }}
             >
                 {progress !== 0 ? `${progress.toFixed(2)}%` : 'Loading...'}
             </p>
--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,15 +1,14 @@
-import { PerspectiveCamera } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
-import HackerRoom from "../components/HackerRoom"
 import { Suspense } from "react"
+
+import { Canvas } from "@react-three/fiber"
+import { PerspectiveCamera } from "@react-three/drei"
 import Loading from "../components/loading"
+import HackerRoom from "../components/HackerRoom"
 
 
 const Hero = () => {
   return (
-    <section
-        className="min-h-screen w-full flex flex-col relative"
-    >
+    <section className="min-h-screen w-full flex flex-col relative">
         <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
             <p 
                 className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans"
@@ -24,7 +23,7 @@ const Hero = () => {
         
         <div className="w-full h-full absolute inset-0">
             <Canvas 
-                className="h-full w-full"
+                className="w-full h-full"
             >
                 <Suspense fallback={<Loading />}>
                     
@@ -33,7 +32,19 @@ const Hero = () => {
                     makeDefault
                     position={[0,0,30]}
                 />
-                <HackerRoom/>
+                <HackerRoom
+                    scale={0.07}
+                    position={[0,0,0]}
+                    rotation={[0,280,0]}
+                />
+
+                <ambientLight
+                    intensity={1}
+                />
+                <directionalLight
+                    position={[10,10,10]}
+                    intensity={0.5}
+                />
             </Suspense>
             </Canvas>
         </div>
