e7eccc53e799062a7eef058ff7c3eae288bdfa67
Bo <boprojectbo@gmail.com>
2024-09-11 16:50:12 +0200

    Feat: Footer/Contact/ Demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import About from "./sections/About";
 import Client from "./sections/Client";
+import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import Projects from "./sections/Projects";
@@ -8,11 +9,12 @@ import Projects from "./sections/Projects";
 const App = () => {
   return ( 
      <main className="max-w-7xl mx-auto">
-       <Navbar />
-       <Hero />
-       <About/>
-       <Projects/>
-      <Client/>
+        <Navbar />
+        <Hero />
+        <About/>
+        <Projects/>
+        <Client/>
+        <Footer/>
      </main>
    );
 }
